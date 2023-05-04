23b65ebc085490a1b7ca3c60e162889d485ec9eb
HybridRbt <littlejack0120@gmail.com>
2023-05-03 21:42:00 -0500

    implement the idea

--- append2DNP.js
+++ append2DNP.js
@@ -1,65 +1,43 @@
-// This is the same as "Go to Today's Journal", but I don't know how
-// to define them in one place and share that information
-var bjTitle = "# Journal for " + Date.today().toString("yyyy-MM-dd");
-var tag = "journal";
+// find today's DNP; create if none exist
+var title = "# " + Date.today().toString("yyyy-MM-dd");
 
-var targetDraftUuid = draft.getTemplateTag("targetDraftUuid");
+var drafts = Draft.query(title, "inbox");
 
-if(targetDraftUuid == "") {
-	var currentContent = editor.getText();
+if (drafts.length > 1) {
+    // if multiple drafts are found, merge them
+    let dNew = Draft.create();
+	dNew.content = drafts.join("\n");
+	dNew.update();
+
+    var target = dNew;
+}
+else if (drafts.length == 0) {
+    // none found, create one
+	var target = Draft.create();
+	target.content = title + "\n\n";
+	target.update();
+	app.displayInfoMessage("Created a new journal: " + title);
 }
 else {
-	var currentContent = Draft.find(targetDraftUuid).content;
+    // just found 1, this is the target
+	var target = drafts[0];
 }
 
-// If the current draft doesn't start with "-", "*", or "@", then
-// add a "- " to the front of it.
-if ("-*@".indexOf(currentContent[0]) == -1) {
-	currentContent = "- " + currentContent;
-}
+//add creation time
+var currentContent = draft.processTemplate("[[created|%H:%M:%S]]\n") + draft.content;
 
 // If the current draft doesn't end with a newline, then add one.
 if (currentContent.slice(-1) != "\n") {
-	currentContent = currentContent + "\n";
+	currentContent = currentContent + "\n\n";
 }
 
-// Look for Today's Journal and complain if there are more than one
-// matches.
-var drafts = Draft.query(bjTitle, "all", [tag]);
-if (drafts.length > 1) {
-	app.displayErrorMessage(drafts.length + " drafts have title '" + bjTitle + "'");
-    context.fail();
+var existingContent = target.content;
+
+// If the existing content doesn't end with a newline, then add one.
+if (existingContent.slice(-1) != "\n") {
+    existingContent = existingContent + "\n\n"
 }
-else {
-    var ourDraft = null;
-    if (drafts.length == 0) {
-    	ourDraft = Draft.create();
-    	ourDraft.content = bjTitle + "\n\n";
-    	ourDraft.addTag(tag);
-    	ourDraft.update();
-    	app.displayInfoMessage("Created a new journal: " + bjTitle);
-    }
-    else if ((targetDraftUuid == drafts[0].uuid) || (targetDraftUuid == "" && draft.uuid == drafts[0].uuid)) {
-    	// It would be bad if we kept adding a draft's contents to itself.
-    	// Keep that up a few times and the world would run out of hard
-    	// drives to store it.
-    	app.displayErrorMessage("Not appending this note to itself.");
-        context.fail();
-    }
-    else {
-        ourDraft = drafts[0];
-   	}
-    
-    if (ourDraft != null) {
-    	var existingContent = ourDraft.content;
-    
-    	// If the existing content doesn't end with a newline, then add one.
-    	if (existingContent.slice(-1) != "\n") {
-    		existingContent = existingContent + "\n"
-    	}
-    
-    	// Append the old draft to Today's Journal and save it.
-    	ourDraft.content = existingContent + currentContent;
-    	ourDraft.update();
-    }
-}
+
+// Append the old draft to Today's Journal and save it.
+target.content = existingContent + currentContent;
+target.update();
